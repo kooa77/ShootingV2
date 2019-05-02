71dfa236e36dd481bdcdaab4d1125e7534af9fc4
kooa <kooa77@gmail.com>
2019-05-03 08:24:02 +0900

    start
    프로젝트 세팅
        기존 프로젝트 복사
        필요한 것만 남기고 삭제
        카메라 탑뷰로 조정
    
    키 입력 수정
    
    총알 발사
    
    총알과 적 충돌 체크
    
    적의 총알 발사

---
+++ Unity/Assets/01. Scripts/MainGameScene/MainGame/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	void Start ()
+    {
+        Destroy(gameObject, 3.0f);
+	}
+
+    float _curTestRot = 0;
+
+    void Update ()
+    {
+        // z 축을 변경 (양의 값으로)
+        /*
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(curPos.x, curPos.y, curPos.z + 0.1f);
+        transform.position = nextPos;
+        */
+
+        // for test
+        transform.position = transform.position + (transform.forward * 0.1f);
+        transform.Rotate(Vector3.up, _curTestRot);
+        _curTestRot -= 0.01f;
+        //_curTestRot = Mathf.Cos(Time.deltaTime) * 100.0f;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
